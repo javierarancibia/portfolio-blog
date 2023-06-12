ee35a0b7a493095727ea9316b883f8bbc55831af
javierarancibia <javier@hodl.nl>
2023-06-12 17:05:13 +0200

    Added canonical site and chile lang to html tag

--- app/layout.js
+++ app/layout.js
@@ -6,11 +6,14 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'CommercialCity: La Nueva Forma de Tasar tu Departamento',
   description: 'Tasaciones 100%',
+  alternates: {
+    canonical: 'https://www.ciudad-comercial.cl',
+  }
 }
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="es-CL">
       <body className={inter.className}>{children}</body>
     </html>
   )
