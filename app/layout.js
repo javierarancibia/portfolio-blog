6be0b8b57b877f0b08919ea46f52ddc544119e12
javierarancibia <javier@hodl.nl>
2023-06-13 11:18:07 +0200

    Metadata update / sitemap / robots.txt

--- app/layout.js
+++ app/layout.js
@@ -6,8 +6,13 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'CommercialCity: La Nueva Forma de Tasar tu Departamento',
   description: 'Tasaciones 100%',
+  keywords: ['Tasaciones Online', 'Como tasar tu departamento', 'Cuanto cuesta mi departamento', 'Tasadores Chile'],
   alternates: {
     canonical: 'https://www.ciudad-comercial.cl',
+    languages: {
+      'es-CL': 'https://www.ciudad-comercial.cl/es-CL',
+      'es': 'https://www.ciudad-comercial.cl/es',
+    },
   }
 }
 
---
+++ app/sitemap.js
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.ciudad-comercial.cl",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.ciudad-comercial.cl/comprar",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.ciudad-comercial.cl/demo",
+      lastModified: new Date(),
+    },
+  ];
+}
