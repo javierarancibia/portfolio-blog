71fc270b1d2bec7e2da658779c25a356b05c40dd
javier <javier@hodl.nl>
2023-07-09 22:03:12 +0200

    BuyButton fixed component

--- app/comprar/BuyButton.js
+++ app/comprar/BuyButton.js
@@ -7,53 +7,51 @@ import styles from "./comprar.module.css"
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
+initMercadoPago(process.env.MERCADO_PAGO_PUBLIC_KEY);
+
+const createPreference = async (customerEmail) => {
+    try {
+        const response = await axios.post(`${process.env.SERVER_URL}/api/v1/payments/create-order`, {email: customerEmail});
+        const id = response.data.data;
+        return id;
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 
 const Product = () => {
-  const [preferenceId, setPreferenceId] = useState(null);
-  const [ customerEmail, setCustomerEmail ] = useState()
+    const [preferenceId, setPreferenceId] = useState(null);
+    const [ customerEmail, setCustomerEmail ] = useState()
 
-  initMercadoPago(process.env.MERCADO_PAGO_PUBLIC_KEY);
-
-  const createPreference = async (customerEmail) => {
-    try {
-      const response = await axios.post(`${process.env.SERVER_URL}/api/v1/payments/create-order`, {email: customerEmail});
-
-      const id = response.data.data;
-      return id;
-    } catch (error) {
-      console.log(error);
+    const handleBuy = async () => {
+        const data = await createPreference(customerEmail);
+        if (data) {
+        setPreferenceId(data.id);
+        }
     }
-  };
 
-  const handleBuy = async () => {
-    const id = await createPreference(customerEmail);
-    if (id) {
-      setPreferenceId(id);
-    }
-  };
-
-  return (
-    <div>
-        <input type="email" placeholder="Ingresa tu correo electrónico" className={styles.emailInput} onChange={e => setCustomerEmail(e.target.email)} /> 
-        <button
-            onClick={handleBuy}
-            className={`hover:bg-blue-300 mt-10 inline-block rounded-full rounded-lg bg-blue-700 px-12 py-3 text-sm font-medium text-white ${inter.className}`}
-            disabled={customerEmail}
-        >
-            Comprar
-        </button>
-        {preferenceId && 
-            <div className={styles.modal}>
-                <div className={styles.modalContent}>
-                    <div className={styles.walletContainer}>
-                        <Wallet initialization={{ preferenceId }} />
+    return (
+        <div>
+            <input type="email" placeholder="Ingresa tu correo electrónico" className={styles.emailInput} onChange={e => setCustomerEmail(e.target.value)} /> 
+            <button
+                onClick={handleBuy}
+                className={`hover:bg-blue-300 mt-10 inline-block rounded-full rounded-lg bg-blue-700 px-12 py-3 text-sm font-medium text-white ${inter.className}`}
+                disabled={!customerEmail}
+            >
+                Comprar
+            </button>
+            {preferenceId && 
+                <div className={styles.modal}>
+                    <div className={styles.modalContent}>
+                        <div className={styles.walletContainer}>
+                            <Wallet initialization={{ preferenceId }} />
+                        </div>
                     </div>
                 </div>
-            </div>
-        }
-    </div>
-  );
+            }
+        </div>
+    );
 };
 
 export default Product;
