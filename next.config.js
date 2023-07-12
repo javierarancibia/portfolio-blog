06439781166688acebd84c6c54ad36ad3a3b2159
javier <javier@hodl.nl>
2023-07-12 17:28:04 +0200

    UPDATED ENV VARS

--- next.config.js
+++ next.config.js
@@ -5,10 +5,7 @@ const nextConfig = {
     domains: ["cdn.shopify.com"],
   },
   env: {
-    STOREFRONT_ACCESS_TOKEN: process.env.STOREFRONT_ACCESS_TOKEN,
-    SHOP_URL: process.env.SHOP_URL,
     MERCADO_PAGO_PUBLIC_KEY: process.env.MERCADO_PAGO_PUBLIC_KEY,
-    CLIENT_URL: process.env.CLIENT_URL,
     SERVER_URL: process.env.SERVER_URL
   },
 };
