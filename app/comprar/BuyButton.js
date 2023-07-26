23689649c3a53ae62e99657dd6e44a182f561d8f
javier <javier@hodl.nl>
2023-07-26 11:27:46 +0200

    Buy Button state handlers w/ messages / Comprar page css update

--- app/comprar/BuyButton.js
+++ app/comprar/BuyButton.js
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { initMercadoPago, Wallet } from "@mercadopago/sdk-react";
 import axios from "axios";
 import styles from "./comprar.module.css"
@@ -23,38 +23,57 @@ const createPreference = async (customerEmail) => {
 const Product = () => {
     const [preferenceId, setPreferenceId] = useState(null);
     const [ customerEmail, setCustomerEmail ] = useState()
+    const [ chosenCity, setChosenCity ] = useState()
+    const [ buttonValidator, setButtonValidator ] = useState(true)
 
     const handleBuy = async () => {
+        let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}')
+        if (regex.test(customerEmail) === false) {
+            return alert("Ingrese un correo válido")
+        }
         const data = await createPreference(customerEmail);
         if (data) {
-        setPreferenceId(data.id);
+            setPreferenceId(data.id);
         }
     }
 
+    useEffect(() => {
+        if (chosenCity && customerEmail) {
+            return setButtonValidator(false)
+        } return setButtonValidator(true)
+    }, [ chosenCity, customerEmail ])
+
     return (
-    <div className="grid grid-rows-3">
+    <div className="grid grid-rows-3 lg:w-full">
         <div>
-            <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-700 focus:border-blue-700 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-700 dark:focus:border-blue-700">
-                <option>Selecciona una comuna</option>
-                <option defaultValue="US">United States</option>
-                <option defaultValue="CA">Canada</option>
-                <option defaultValue="FR">France</option>
-                <option defaultValue="DE">Germany</option>
+            <select 
+                id="cities" 
+                className="bg-gray-50 outline-blue-500 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-700 focus:border-blue-700 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-700 dark:focus:border-blue-700"
+                onChange={e => setChosenCity(e.target.value)}
+            >
+                <option value="">Selecciona una comuna</option>
+                <option value="Providencia">Providencia</option>
+                <option value="Santiago">Santiago Centro</option>
+                <option value="Maipu">Maipú</option>
+                <option value="San Miguel">San Miguel</option>
             </select>
+            { chosenCity && <p className={`mt-3 italic text-xs font-bold text-blue-700 ${inter.className}`}>¡Perfecto! Contamos con datos suficientes para la comuna de {chosenCity}</p> }
         </div>
         <div>
             <input 
                 type="email" 
                 placeholder="Ingresa tu correo electrónico" 
                 // className={styles.emailInput} 
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required
-                onChange={e => setCustomerEmail(e.target.value)} /> 
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 outline-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                onChange={e => setCustomerEmail(e.target.value)} 
+            /> 
+            <p className={`mt-6 italic text-xs font-thin leading-4 text-white ${inter.className}`}>* Asegúrate de ingresar la dirección de correo correcta para enviarte el link al formulario después de la compra</p>
         </div>
         <div>
             <button
                 onClick={handleBuy}
-                className={`hover:bg-blue-300 mt-10 inline-block rounded-full rounded-lg bg-blue-700 px-12 py-3 text-sm font-medium text-white ${inter.className}`}
-                disabled={!customerEmail}
+                className={`mt-10 inline-block rounded-full rounded-lg bg-blue-700 px-12 py-3 text-sm font-medium text-white ${inter.className}`}
+                disabled={buttonValidator}
             >
                 Comprar
             </button>
--- app/comprar/page.js
+++ app/comprar/page.js
@@ -8,9 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export default async function Comprar() {
 
   return (
-    <section className="relative flex flex-wrap lg:h-screen lg:items-center bg-gray-800">
-      <div className="w-full px-4 py-12 sm:px-6 sm:py-16 lg:w-1/2 lg:px-12 lg:py-24">
-        <div className="mx-auto max-w-md text-center lg:mx-15 lg:flex-auto lg:py-32 lg:text-left">
+    <section className="relative flex flex-wrap lg:items-center bg-gray-800">
+      <div className="w-full px-4 py-12 sm:px-6 sm:py-16 lg:w-1/2 lg:px-12 lg:py-24 lg:-mt-14">
+        <div className="mx-auto max-w-md text-center lg:mx-15 lg:flex-auto lg:py-12 lg:text-left">
         <div className={styles.logo} >
             <Image src="/logowhite.png" height={100} width={100} className="mb-5 lg:mb-12 w-[5rem] max-w-none sm:w-[7rem] md:-ml-4 lg:-ml-0" unoptimized alt="hero"  />
         </div>
@@ -22,16 +22,15 @@ export default async function Comprar() {
             Obtén el valor de tu departamento en segundos
           </h2>
           <p className={ `mt-6 text-lg leading-8 text-gray-300 ${inter.className}`}>
-          ¡No pierdas más tiempo y dinero! Descubre el poder de CommercialCity y obtén tasaciones precisas al instante.
+            ¡No pierdas más tiempo y dinero! Descubre el poder de CommercialCity y obtén tasaciones precisas al instante.
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
             <BuyButton />
           </div>
-          <p className={`mt-6 italic text-sm font-thin leading-7 text-white ${inter.className}`}>* Asegúrate de ingresar la dirección de correo correcta para enviarte el link al formulario después de la compra</p>
         </div>
       </div>
 
-      <div className="relative h-64 w-full sm:h-96 lg:h-full lg:w-1/2" style={{marginTop:"12rem"}}>
+      <div className="relative h-64 w-full sm:h-96 lg:h-full lg:w-1/2" >
         <Image
           src="/mockup4.png"
           height={1000}
